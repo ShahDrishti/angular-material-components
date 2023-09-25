c3e39545a8f216b33837cfc409b6d3c78b5ab63b
Smruti Shah <drishtishah2004@gmail.com>
2023-09-25 22:38:16 +0530

    added 2 folders and practiced routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppComponent } from './app.component';
+import { MaterialTableComponent } from './material-table/material-table.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialTableComponent,
+    ReactiveFormComponent
   ],
   imports: [ 
     BrowserModule,
