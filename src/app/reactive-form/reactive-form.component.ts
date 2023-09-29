48baab31cb42b65e3ed5cf9770cbfbf7718cb7e6
Smruti Shah <drishtishah2004@gmail.com>
2023-09-29 20:02:26 +0530

    Almost at the end of reactive form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -10,15 +10,44 @@ import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
 export class ReactiveFormComponent implements OnInit {
     loginForm: FormGroup = new FormGroup({
       userData :new FormGroup({
-        name : new FormControl(null,
-          Validators.required),
+        name : new FormControl(
+          null,
+          [Validators.required]),
         email: new FormControl(
           null,
-          [Validators.required, Validators.email]
+          [Validators.required, Validators.email],
+          ),
+        password: new FormControl(null,
+          [Validators.required,Validators.minLength(8)]
           )
+          
       }),
     })
     constructor(private fb: FormBuilder){}
 
-    ngOnInit(){}
+    ngOnInit(){
+      this.loginForm.setValue({
+        userData:{
+          name:'max',
+          email:'max@gmail.com',
+          password:''
+        } 
+
+
+      })
+      this.loginForm.statusChanges.subscribe((status) => console.log(status));
+    };
+    
+
+    onContinue(){
+      if(this.loginForm.valid)
+      {
+       console.log(this.loginForm);
+       this.loginForm.reset();
+      }
+    }
+
+    get passwordControl(){
+      return this.loginForm.get('userData.password');
+    }
 }
